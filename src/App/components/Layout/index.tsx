f611c6db677ae02e098f11094242fa08e6dc4def
Ignacio Bejarano <ignbejarano@gmail.com>
2021-03-17 19:31:49 -0300

    basic layout implementation

---
+++ src/App/components/Layout/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+import logo from "../../../assets/logo.svg";
+import styles from "./Layout.module.scss";
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <main className={styles.container}>
+      <nav>
+        <img alt="Aerolab" src={logo} />
+        <p>John Kite</p>
+      </nav>
+      <figure>
+        <h2>Electronics</h2>
+      </figure>
+      <section>{children}</section>
+    </main>
+  );
+};
+
+export default Layout;
