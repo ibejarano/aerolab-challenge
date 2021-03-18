f52b39c7d2e22766d57a85a56e1293f53947058c
Ignacio Bejarano <ignbejarano@gmail.com>
2021-03-18 17:52:45 -0300

    styled available product

--- src/App/components/ProductsList/Product.tsx
+++ src/App/components/ProductsList/Product.tsx
@@ -2,15 +2,25 @@ import * as React from "react";
 import styles from "./Product.module.scss";
 
 const Product: React.FC = ({ name, cost, _id, img, points }) => {
-  return (
-    <div key={_id} className={styles.container}>
-      <img alt={name} src={img.url} />
-      {points > cost ? (
+  if (cost > points) {
+    return (
+      <div key={_id} className={styles.containerUnavailable}>
+        <img alt={name} src={img.url} />
         <h3>Cost: {cost}</h3>
-      ) : (
-        <h3>You need {cost - points} more points</h3>
-      )}
-      <h3>{name}</h3>
+        <h3>You need {cost - points} more</h3>
+        <p>{name}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div key={_id} className={styles.containerAvailable}>
+      <h3>Cost: {cost}</h3>
+      <img alt={name} src={img.url} />
+      <p>{name}</p>
+      <div className={styles.redeem}>
+        <button>Redeem now!</button>
+      </div>
     </div>
   );
 };
