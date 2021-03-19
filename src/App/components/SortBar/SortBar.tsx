d1b916db983eefe1aa759006fffc50d9c208390c
Ignacio Bejarano <ignbejarano@gmail.com>
2021-03-19 11:06:27 -0300

    added sort recent

--- src/App/components/SortBar/SortBar.tsx
+++ src/App/components/SortBar/SortBar.tsx
@@ -5,19 +5,26 @@ const SortBar: React.FC = ({ products, setProducts, setSort }) => {
   const sortByAscendingPrice = () => {
     const sorted = products.sort((a, b) => (a.cost > b.cost ? 1 : -1));
     setProducts(sorted);
-    setSort("AscendingPrice");
+    setSort("ascendingPrice");
   };
 
   const sortByDescendingPrice = () => {
     const sorted = products.sort((a, b) => (a.cost < b.cost ? 1 : -1));
     setProducts(sorted);
-    setSort("DescendingPrice");
+    setSort("descendingPrice");
+  };
+
+  const sortByRecent = () => {
+    const sorted = products.sort((a, b) => (a._id > b._id ? 1 : -1));
+    setProducts(sorted);
+    setSort("recent");
   };
 
   return (
     <div className={container}>
       <span>32 products of 32: (hardcoded)</span>
       <div>
+        <button onClick={sortByRecent}>Most Recent</button>
         <button onClick={sortByDescendingPrice}>Highest Price</button>
         <button onClick={sortByAscendingPrice}>Lowest Price</button>
       </div>
