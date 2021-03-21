8f94f6ed3b412cad93424564bad21c41e4e9d904
Ignacio Bejarano <ignbejarano@gmail.com>
2021-03-21 14:03:46 -0300

    fix SortBar types

--- src/App/components/SortBar/SortBar.tsx
+++ src/App/components/SortBar/SortBar.tsx
@@ -1,50 +1,63 @@
 import * as React from "react";
-import {
-  container,
-  paginationContainer,
-  sortContainer,
-} from "./SortBar.module.scss";
+import styles from "./SortBar.module.scss";
 
 import left from "../../../assets/icons/arrow-left.svg";
 import right from "../../../assets/icons/arrow-right.svg";
 
-const SortBar: React.FC = ({ allProducts, setSort, currPage, setCurrPage }) => {
+enum SortType {
+  Recent = "recent",
+  AscendingPrice = "ascendingPrice",
+  DescendingPrice = "descendingPrice",
+}
+interface Props {
+  allProducts: any[];
+  currPage: number;
+  setSort: (sortType: SortType) => void;
+  setCurrPage: (page: (prevPagE: number) => number) => void;
+}
+
+const SortBar: React.FC<Props> = ({
+  allProducts,
+  currPage,
+  setSort,
+  setCurrPage,
+}) => {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   const sortByRecent = () => {
     allProducts = allProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
-    setSort("recent");
+    setSort(SortType.Recent);
     setSelectedIndex(0);
   };
   const sortByDescendingPrice = () => {
     allProducts = allProducts.sort((a, b) => (a.cost < b.cost ? 1 : -1));
-    setSort("descendingPrice");
+    setSort(SortType.DescendingPrice);
     setSelectedIndex(1);
   };
   const sortByAscendingPrice = () => {
     allProducts = allProducts.sort((a, b) => (a.cost > b.cost ? 1 : -1));
-    setSort("ascendingPrice");
+    setSort(SortType.AscendingPrice);
     setSelectedIndex(2);
   };
 
   return (
-    <div className={container}>
-      <div className={paginationContainer}>
+    <div className={styles.container}>
+      <div className={styles.paginationContainer}>
         <span>{(currPage + 1) * 16} products of 32</span>
         <button
-          onClick={() => setCurrPage((page: number) => page - 1)}
+          onClick={() => setCurrPage((page) => page - 1)}
           disabled={currPage == 0}
         >
           <img src={left} alt="left arrow" />
         </button>
         <button
-          onClick={() => setCurrPage((page: number) => page + 1)}
+          onClick={() => setCurrPage((page) => page + 1)}
           disabled={currPage == 1}
         >
           <img src={right} alt="right arrow" />
         </button>
       </div>
-      <div className={sortContainer}>
+      <div className={styles.sortContainer}>
         <p>Sort by:</p>
         <button onClick={sortByRecent} disabled={selectedIndex == 0}>
           Most Recent
